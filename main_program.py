4a4bd977bdf361d391000c663605219484f79f88
minhquan1313 <minhquan1313@gmail.com>
2026-05-27 13:19:28 +0700

    added auto start bot added auto focus interval 5s

--- main_program.py
+++ main_program.py
@@ -4,7 +4,9 @@ from typing import Callable
 import asyncio
 import pyautogui
 import settings
-import os 
+import os
+import win32gui
+import win32con
 
 intents = discord.Intents.default()
 pyautogui.FAILSAFE = False
@@ -15,15 +17,53 @@ async def load_cogs():
         print(filename)
         if filename.endswith(".py"):
             await bot.load_extension(f"source.discord_commands.{filename[:-3]}")
-            
+
+focus_window_task = None
+
+
+def focus_window(window_title="ArkAscended"):
+    global focus_window_task
+
+    if focus_window_task and not focus_window_task.done():
+        return
+
+    async def callback():
+        while True:
+            try:
+                hwnd = win32gui.FindWindow(None, window_title)
+                if hwnd:
+                    if win32gui.GetForegroundWindow() != hwnd:
+                        win32gui.ShowWindow(hwnd, win32con.SW_RESTORE)
+                        win32gui.SetForegroundWindow(hwnd)
+
+                    await asyncio.sleep(0.1)
+            except Exception as e:
+                print(f"Error focusing window: {e}")
+            await asyncio.sleep(5)
+
+    focus_window_task = asyncio.create_task(callback())
+
+
+bot_started_once = False
+
+
 @bot.event
 async def on_ready():
+    global bot_started_once
     await bot.tree.sync()
-    
+
     logchn = bot.get_channel(int(settings.log_channel_gacha))
     if logchn:
-        await logchn.send(f'bot ready to start')
-    print (f'logged in as {bot.user}')
+        await logchn.send(f"bot ready to start")
+        if not bot_started_once:
+            bot_started_once = True
+
+            cog = bot.get_cog("discord_commands")
+            if cog:
+                bot.loop.create_task(cog.start_bot_core(wait=0))
+    print(f"logged in as {bot.user}")
+    focus_window("ArkAscended")
+
 
 api_key = settings.discord_api_key
 
